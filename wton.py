6c227c460cf368197fa60e78188a4e0f5118a8d1
Tanuj Yadav <tanujyadav171997@gmail.com>
2018-10-07 00:19:58 +0530

    Updated wton.py
    1- Added logic to consider uppercase input by user
    2- Reformatted code
    3- Removed empty strings from tens array
    4- Removed redundant if condition

--- wton.py
+++ wton.py
@@ -1,24 +1,24 @@
 def text2int(textnum, numwords={}):
     if not numwords:
-      units = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen",
-      ]
+        units = [
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen", "eighteen", "nineteen",
+        ]
 
-      tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+        tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-      scales = ["hundred", "thousand", "million", "billion", "trillion"]
+        scales = ["hundred", "thousand", "million", "billion", "trillion"]
 
-      numwords["and"] = (1, 0)
-      for idx, word in enumerate(units):    numwords[word] = (1, idx)
-      for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
-      for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+        numwords["and"] = (1, 0)
+        for idx, word in enumerate(units):    numwords[word] = (1, idx)
+        for idx, word in enumerate(tens):     numwords[word] = (1, (idx + 2) * 10)
+        for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
 
     current = result = 0
     for word in textnum.split():
         if word not in numwords:
-          raise Exception("Illegal word: " + word)
+            raise Exception("Illegal word: " + word)
 
         scale, increment = numwords[word]
         current = current * scale + increment
@@ -28,52 +28,54 @@ def text2int(textnum, numwords={}):
 
     return result + current
 
+
 check = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety","hundred", "thousand", "million", "billion", "trillion"
-      ]
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+    "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+    "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
+    "ninety", "hundred", "thousand", "million", "billion", "trillion"
+]
 
-text="twenty two thousand pound"
+text = "Twenty two thousand pound"
+text = text.lower()  # to make the code work even if user write some uppercase characters
+a = text.split(" ")
+f1 = 0
+j = 1
 
-a=text.split(" ")
-f1=0
-j=1
+if a[-1] == "cent" or a[-1] == "cents":
+    f1 = 1
+    text = text[:len(text) - (len(a[-1]) + 1)]
+    for i in check:
+        if i == a[-3]:
+            j = 2
+            break
 
-if(a[-1]=="cent" or a[-1]=="cents"):
-  f1=1
-  text=text[:len(text)-(len(a[-1])+1)]
-  for i in check:
-    if(i==a[-3]):
-      j=2
-      break
+    if j == 2:
+        text1 = a[-3] + " " + a[-2]
+        text = text[:len(text) - (len(a[-2]) + len(a[-3]) + 1)]
+    if j == 1:
+        text1 = a[-2]
+        text = text[:len(text) - (len(a[-2]) + 1)]
+    text = text[:len(text) - 1]
 
-  if(j==2):
-    text1=a[-3]+" "+a[-2]
-    text=text[:len(text)-(len(a[-2])+len(a[-3])+1)]
-  if(j==1):
-    text1=a[-2]
-    text=text[:len(text)-(len(a[-2])+1)]
-  text=text[:len(text)-1]
-
-a=text.split(" ")
-f=0
+a = text.split(" ")
+f = 0
 for i in check:
-  if(i==a[-1]):
-    f=1
+    if i == a[-1]:
+        f = 1
 
-if(f!=1):
-  text=text[:len(text)-(len(a[-1])+1)]
-  if(a[-1]=="dollar" or a[-1]=="dollars"):
-    currency="$"
-  if(a[-1]=="euro" or a[-1]=="euros"):
-    currency = u"\u20ac"
-  if(a[-1]=="pound" or a[-1]=="pounds"):
-    currency=u"\u00A3"
+currency = ""
+if f != 1:
+    text = text[:len(text) - (len(a[-1]) + 1)]
+    if a[-1] == "dollar" or a[-1] == "dollars":
+        currency = "$"
+    if a[-1] == "euro" or a[-1] == "euros":
+        currency = u"\u20ac"
+    if a[-1] == "pound" or a[-1] == "pounds":
+        currency = u"\u00A3"
 
-if(f1!=1):
-  final= str(text2int(text))+currency
-  print(final)
+    final = str(text2int(text)) + currency
+    print(final)
 else:
-  final= str(text2int(text))+"."+str(text2int(text1))+currency
-  print(final)
+    final = str(text2int(text)) + "." + str(text2int(text1)) + currency
+    print(final)
